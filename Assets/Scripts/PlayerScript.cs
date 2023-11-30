ed7f1c68786e1ee435b80046549d555a036edce4
Minh Nguyen <melnguyen115@gmail.com>
2023-11-30 14:38:09 -0700

    Added collision layer and tweaked PlayerScript to stop movement on wall collision

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -7,6 +7,7 @@ public class PlayerScript : MonoBehaviour
     public static PlayerScript Instance;
     [SerializeField] protected float playerSpeed;
     [SerializeField] protected Transform destination;
+    [SerializeField] protected LayerMask collisionMask;
 
     void Awake() {
         if (Instance == null) {
@@ -24,17 +25,24 @@ public class PlayerScript : MonoBehaviour
         // Move Player to destination point
         transform.position = Vector3.MoveTowards(transform.position, destination.position, playerSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, destination.position) <= .05f)
+        if (Vector3.Distance(transform.position, destination.position) <= .05f) // Check if Player is near destination
         {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f)
             {
-                destination.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                if (!Physics2D.OverlapCircle(destination.position + new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f), .1f, collisionMask)) // Check if new destination position will overlap with collisionMask
+                {
+                    destination.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+                }
             }
 
             if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f)
             {
-                destination.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                if (!Physics2D.OverlapCircle(destination.position + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f), .1f, collisionMask))
+                {
+                    destination.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+                }
+              
             }
         }
     }
