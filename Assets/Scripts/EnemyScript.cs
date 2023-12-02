7365e942f5489acda6124cd497f362a50e444b00
Alan Cheng <aaaalancheng@gmail.com>
2023-12-01 20:02:39 -0700

    pathfinding isn't working right now

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -1,15 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
+using Toolbox;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class EnemyScript : MonoBehaviour {
     [SerializeField] protected float enemySpeed;
     [SerializeField] protected Transform enemyDestination;
     [SerializeField] protected Transform playerPosition;
+    [SerializeField] protected Tilemap tileMap;
+    private List<Vector3> nextMove;
 
     void Start() {
         enemyDestination.parent = null;
         playerPosition = PlayerScript.Instance.getMovePoint();
+        tileMap = GameObject.Find("Ground").GetComponent<Tilemap>();
+        nextMove = new List<Vector3>();
     }
 
     void Update() {
@@ -17,18 +23,21 @@ public class EnemyScript : MonoBehaviour {
     }
 
     public void EnemyMove() {
-        Vector3 pathToPlayer = playerPosition.position - enemyDestination.position;
+        nextMove = AStar.FindPathClosest(tileMap, enemyDestination.position, playerPosition.position);
+        Debug.Log("nextMove: " + nextMove); // nextMove is null
 
-        if (pathToPlayer.x > 0) {
-            enemyDestination.position += new Vector3(1f, 0f, 0f);
-        } else if (pathToPlayer.x < 0) {
-            enemyDestination.position += new Vector3(-1f, 0f, 0f);
-        }
+        // Vector3 pathToPlayer = playerPosition.position - enemyDestination.position;
 
-        if (pathToPlayer.y > 0) {
-            enemyDestination.position += new Vector3(0f, 1f, 0f);
-        } else if (pathToPlayer.y < 0) {
-            enemyDestination.position += new Vector3(0f, -1f, 0f);
-        }
+        // if (pathToPlayer.x > 0) {
+        //     enemyDestination.position += new Vector3(1f, 0f, 0f);
+        // } else if (pathToPlayer.x < 0) {
+        //     enemyDestination.position += new Vector3(-1f, 0f, 0f);
+        // }
+
+        // if (pathToPlayer.y > 0) {
+        //     enemyDestination.position += new Vector3(0f, 1f, 0f);
+        // } else if (pathToPlayer.y < 0) {
+        //     enemyDestination.position += new Vector3(0f, -1f, 0f);
+        // }
     }
 }
