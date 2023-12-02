8da17530f6ef96e5c8604d383491fba9cb8d16e8
Minh Nguyen <melnguyen115@gmail.com>
2023-12-02 13:22:03 -0700

    added behavior so projectile destroys on terrain collision

--- Assets/Scripts/ProjectileBehaviorScript.cs
+++ Assets/Scripts/ProjectileBehaviorScript.cs
@@ -3,20 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ProjectileBehaviorScript : MonoBehaviour {
-
     [SerializeField] protected float projectileSpeed;
     [SerializeField] protected float projectileDistance;
     [SerializeField] protected Transform projectileDestination;
+    [SerializeField] protected LayerMask collisionMask;
     private int direction = 0;
+    private GameObject projectileMovePoint;
 
     // Start is called before the first frame update
     void Start() {
+        this.projectileMovePoint = projectileDestination.gameObject;
         projectileDestination.parent = null;
-        Destroy(this.gameObject, 10);
+
+        // will destroy the projectile by default after 20 seconds
+        Destroy(this.gameObject, 20);
+        Destroy(this.projectileMovePoint, 20);
     }
 
     void Update() {
-       transform.position = Vector3.MoveTowards(transform.position, projectileDestination.position, projectileSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, projectileDestination.position, projectileSpeed * Time.deltaTime);
+        
+        if (Physics2D.OverlapCircle(transform.position, .1f, collisionMask)) { // Check if projectile position will overlap with collisionMask
+            Destroy(this.gameObject);
+            Destroy(this.projectileMovePoint);
+        }
     }
 
     public void ProjectileMove() {
@@ -34,8 +44,6 @@ public class ProjectileBehaviorScript : MonoBehaviour {
                 projectileDestination.position += new Vector3(projectileDistance, 0f, 0f);
                 break;
         }
-
-        
     }
 
     public void setDirection(int newDirection) {
