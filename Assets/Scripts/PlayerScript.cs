04a6ad066e79f1118b01327a7364874d1c2960fd
Alan Cheng <aaaalancheng@gmail.com>
2023-12-03 13:03:35 -0700

    Revamp movement, add dashing

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,4 +1,3 @@
-
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +7,18 @@ public class PlayerScript : MonoBehaviour {
     [SerializeField] protected float playerSpeed;
     [SerializeField] protected Transform destination;
     [SerializeField] protected LayerMask collisionMask;
+    [SerializeField] protected float dashSpeed;
+    [SerializeField] protected float dashTiming = 0.4f;
+    
+    private float currentSpeed;
+
+    private bool isHorizontalAxisInUse = false;
+    private bool isVerticalAxisInUse = false;
+
+    private float lastInitialDirectionalInputTime;
+    private bool playerInAction = false;
+    private Vector3 currentMoveDir;
+    private bool hasDashed = false;
 
     void Awake() {
         if (Instance == null) {
@@ -15,81 +26,81 @@ public class PlayerScript : MonoBehaviour {
         }
     }
 
-    int frameCnt;
-
-    int diagonalTolerance = 5; //in frames
-
-    bool movingHori,movingVert;
-
-    bool movementlockedIn;
-
     void Start() {
         destination.parent = null;
-        movingHori = false;
-        movingVert = false;
-        movementlockedIn = false;
-        targetPos = destination.position;
-        frameCnt = diagonalTolerance;
+        currentSpeed = playerSpeed;
     }
 
-    Vector3 targetPos;
-    public bool wallAtPos(Vector3 pos) {
-        return Physics2D.OverlapCircle(pos, .1f, collisionMask);
-    }
     void Update() {
         // Move Player to destination point
-        transform.position = Vector3.MoveTowards(transform.position, destination.position, playerSpeed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, destination.position, currentSpeed * Time.deltaTime);
 
-        if ( Vector3.Distance(transform.position, destination.position) <= .05f || frameCnt<diagonalTolerance) {   // Check if Player is near destination or 
-            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f) {
-                if (!wallAtPos(targetPos + new Vector3(Input.GetAxisRaw("Horizontal"),0,0)) && !movingHori) { // Check if new destination position will overlap with collisionMask
-                    Vector3 horiDir =  new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-                    movingHori = true;
-                    if (movingVert) {
-                        targetPos += horiDir;
-                        movementlockedIn = true;
-                    } else {
-                        targetPos = destination.position + horiDir;
+        // isHorizontalAxisInUse and isVerticalAxisInUse make GetAxisRaw behave like GetKey instead of GetKeyDown
+
+        // Check if a second input has been inputted while player is mid move within the dash timing window
+        // Dash can only be performed once per action
+        if (playerInAction && Time.time - lastInitialDirectionalInputTime < dashTiming && !hasDashed) {
+            if ((Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && !isHorizontalAxisInUse) || (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && !isVerticalAxisInUse)) {
+                isHorizontalAxisInUse = Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f;
+                isVerticalAxisInUse = Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f;
+                Vector3 dashDir = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+                if (dashDir == currentMoveDir) {
+                    hasDashed = true;
+                    if (!Physics2D.OverlapCircle(destination.position + dashDir, 0.1f, collisionMask)) {
+                        Debug.Log("dashing! " + currentMoveDir);
+                        destination.position += dashDir;
+                        currentSpeed = dashSpeed;
                     }
-                    frameCnt = 0;
-                } 
+                }
             }
-
-            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) {
-                if (!wallAtPos(targetPos + new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f)) && !movingVert) {
-                    Vector3 vertDir = new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
-                    movingVert = true;
-                    if (movingHori) {
-                        targetPos += vertDir;
-                        movementlockedIn = true;
-                    } else {
-                        targetPos = destination.position + vertDir;
-                    }
-                    frameCnt = 0;
-                } 
+            if (Input.GetAxisRaw("Horizontal") == 0) {
+                isHorizontalAxisInUse = false;
+            }
+            if (Input.GetAxisRaw("Vertical") == 0) {
+                isVerticalAxisInUse = false;
             }
         }
 
-        if (frameCnt>=diagonalTolerance || movementlockedIn) { // Player has diagonalTolerance # of frames to change their input to a diagonal
-            if (movingHori || movingVert) {
-                destination.position = targetPos;
-                if (EnemyManagerScript.Instance!=null) {
-                    EnemyManagerScript.Instance.EnemyTurn();
-                }
-                if (ProjectileManagerScript.Instance!=null) {
-                    ProjectileManagerScript.Instance.ProjectileTurn();
+        // Check for new input if Player is close enough to destination
+        if (Vector3.Distance(transform.position, destination.position) <= .05f) {
+            // Player character has finished performing action
+            // NPCs take turns, reset everything to listen for new move input
+            if (playerInAction) {
+                Debug.Log("arrived!");
+
+                EnemyManagerScript.Instance.GetComponent<EnemyManagerScript>().EnemyTurn();
+                ProjectileManagerScript.Instance.GetComponent<ProjectileManagerScript>().ProjectileTurn();
+
+                lastInitialDirectionalInputTime = 0f;
+                Debug.Log("lastInitialDirectionalIinputTime: " + lastInitialDirectionalInputTime);
+                playerInAction = false;
+                currentMoveDir = Vector3.zero;
+                hasDashed = false;
+                currentSpeed = playerSpeed;
+            }
+
+            if ((Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f && !isHorizontalAxisInUse) || (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f && !isVerticalAxisInUse)) {
+                isHorizontalAxisInUse = Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f;
+                isVerticalAxisInUse = Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f;
+                Vector3 moveDir = new(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f);
+                // Ensure player doesn't move into wall
+                if (!Physics2D.OverlapCircle(destination.position + moveDir, 0.1f, collisionMask)) {
+                    destination.position += moveDir;
+                    lastInitialDirectionalInputTime = Time.time;
+                    Debug.Log("lastInitialDirectionalIinputTime: " + lastInitialDirectionalInputTime);
+                    playerInAction = true;
+                    currentMoveDir = moveDir;
                 }
             }
-            movingHori = false;
-            movingVert = false;
-            frameCnt = diagonalTolerance;
-            movementlockedIn = false;
-        } else {
-            frameCnt++;
+            if (Input.GetAxisRaw("Horizontal") == 0) {
+                isHorizontalAxisInUse = false;
+            }
+            if (Input.GetAxisRaw("Vertical") == 0) {
+                isVerticalAxisInUse = false;
+            }
         }
+
     }
-
-    
 
     // Move Point getter method
     public Transform getMovePoint() {
