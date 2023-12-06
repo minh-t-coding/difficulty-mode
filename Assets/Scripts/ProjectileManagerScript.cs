d67aff12cd18dc951cdf971305294d288446596b
Minh Nguyen <melnguyen115@gmail.com>
2023-12-06 01:09:31 -0700

    removed more unneeded code

--- Assets/Scripts/ProjectileManagerScript.cs
+++ Assets/Scripts/ProjectileManagerScript.cs
@@ -34,7 +34,6 @@ public class ProjectileManagerScript : MonoBehaviour
                 }
             }
         }
-        notMovingProjectileCount = 0;
     }
 
     public bool getAreProjectilesInAction() {
