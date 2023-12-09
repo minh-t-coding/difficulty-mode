560fbeb81304ce62615021a051540dfee989ad6d
Oshadha Gunasekara <oshadha123@gmail.com>
2023-12-09 12:37:37 -0700

    resolve merge conflicts

--- Assets/Scripts/Behaviors/MeleeEnemyBehaviorScript.cs
+++ Assets/Scripts/Behaviors/MeleeEnemyBehaviorScript.cs
@@ -5,8 +5,39 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 public class MeleeEnemyBehaviorScript : BaseEnemyBehavior {
+    [SerializeField] protected float movementRange;
+    [SerializeField] protected float attackRange;
+    
+    public override bool EnemyInRange() {
+        Vector3 distanceFromPlayer = playerPosition.position - enemyDestination.position;
+        return distanceFromPlayer.magnitude <= attackRange;
+    }
+    
     public override void EnemyMove() {
-        base.EnemyMove();
+        // reset move flag
+        isMoving = false;
+        
+        // check if enemy is already in range
+        Vector3 distanceFromPlayer = playerPosition.position - enemyDestination.position;
+        if (distanceFromPlayer.magnitude < movementRange) {
+            //move enemy so they are top/bottom/left/right of player
+            Vector3 newPosition;
+            if (Mathf.Abs(distanceFromPlayer.x) < Mathf.Abs(distanceFromPlayer.y)) {
+                newPosition = enemyDestination.position + new Vector3(distanceFromPlayer.x / Mathf.Abs(distanceFromPlayer.x), 0);
+            }
+            else {
+                newPosition = enemyDestination.position + new Vector3(0, distanceFromPlayer.y / Mathf.Abs(distanceFromPlayer.y));
+            }
+
+            // only alter path if there is no collision 
+            if (tileMap.IsCellEmpty(newPosition)) {
+                isMoving = true;
+                movePath = newPosition - enemyDestination.position;
+                enemyDestination.position = newPosition;
+            }
+        } else {
+            base.EnemyMove();
+        }
 
         if (isMoving) {
             // add movement command
@@ -34,13 +65,16 @@ public class MeleeEnemyBehaviorScript : BaseEnemyBehavior {
 
     public override void EnemyAttack()
     {
-        base.EnemyAttack();
-
-        // add player attack command
-        List<CommandManager.ICommand> commandList = new List<CommandManager.ICommand>
-        {
-            new MeleeEnemyAttackCommand(new Vector3(0, 0, 0), this)
-        };
-        CommandManager.Instance.AddCommand(this.GetInstanceID(), commandList);
+        if (EnemyInRange()) {
+            HitEffect.CreateHitEffectStatic(playerPosition.position, new Color(1, 0, 0, 1));
+            PlayerBehaviorScript.Instance.killPlayer();
+            
+            // add player attack command
+            List<CommandManager.ICommand> commandList = new List<CommandManager.ICommand>
+            {
+                new MeleeEnemyAttackCommand(new Vector3(0, 0, 0), this)
+            };
+            CommandManager.Instance.AddCommand(this.GetInstanceID(), commandList);
+        }   
     }
 }
