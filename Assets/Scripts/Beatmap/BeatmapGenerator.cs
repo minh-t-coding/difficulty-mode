6266093e7d0f1fb987d4145c60901af573ee34ea
Oshadha Gunasekara <oshadha123@gmail.com>
2023-12-10 17:14:07 -0700

    use two inputs at same time for diagonal movement

--- Assets/Scripts/Beatmap/BeatmapGenerator.cs
+++ Assets/Scripts/Beatmap/BeatmapGenerator.cs
@@ -3,32 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using MEC;
 using System.Linq;
+using UnityEngine.AI;
+using UnityEditor.Experimental.GraphView;
+using System.Security.Cryptography;
 public class BeatmapGenerator : MonoBehaviour
 {   
     [SerializeField] protected float beatmapDelay;
     
     protected Beatmap beatmap;
 
-    private KeyCode getDirectionKey(Vector3 direction) {
-        if (direction == new Vector3(1, 1)) {
-            return KeyCode.E;
-        } else if (direction == new Vector3(1, -1)) {
-            return KeyCode.C;
-        } else if (direction == new Vector3(-1, 1)) {
-            return KeyCode.Q;
-        } else if (direction == new Vector3(-1, -1)) {
-            return KeyCode.Z;
-        } else if (direction == new Vector3(1, 0)) {
-            return KeyCode.A;
-        } else if (direction == new Vector3(-1, 0)) {
-            return KeyCode.D;
-        } else if (direction == new Vector3(0, 1)) {
-            return KeyCode.W;
-        } else if (direction == new Vector3(0, -1)) {
-            return KeyCode.S;
+    // can return multiple keys in the case of diagonal movements
+    private List<KeyCode> getDirectionKeys(Vector3 direction) {
+        List<KeyCode> keys = new List<KeyCode>();
+        
+        if (direction.x == 1f) {
+            keys.Add(KeyCode.D);
+        } else if (direction.x == -1f) {
+            keys.Add(KeyCode.A);
         }
 
-        return KeyCode.S;
+        if (direction.y == 1f) {
+            keys.Add(KeyCode.W);
+        } else if (direction.y == -1f) {
+            keys.Add(KeyCode.S);
+        }
+
+        return keys;
     }
 
     public Beatmap GetBeatmap() {
@@ -48,14 +48,27 @@ public class BeatmapGenerator : MonoBehaviour
             Vector3 direction = state.getDirection();
 
             if (action == PlayerState.PlayerAction.Move) {
-                keys.Add(getDirectionKey(direction));
-                hits.Add(curHit);
+                List<KeyCode> keyCodes = getDirectionKeys(direction);
+                
+                foreach(KeyCode key in keyCodes) {
+                    keys.Add(key);
+                    hits.Add(curHit);
+                }
+
                 curHit += 1;
             } else if (action == PlayerState.PlayerAction.Dash) {
-                keys.Add(getDirectionKey(direction));
-                keys.Add(getDirectionKey(direction));
-                hits.Add(curHit);
-                hits.Add(curHit + (float)0.5);
+                List<KeyCode> keyCodes = getDirectionKeys(direction);
+                
+                foreach(KeyCode key in keyCodes) {
+                    keys.Add(key);
+                    hits.Add(curHit);
+                }
+
+                foreach(KeyCode key in keyCodes) {
+                    keys.Add(key);
+                    hits.Add(curHit + 0.5f);
+                }
+
                 curHit += 1;
             } else if (action == PlayerState.PlayerAction.Attack) {
                 keys.Add(KeyCode.Return);
