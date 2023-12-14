0883473ac144e838d8f701d9ac4072c79119c6d4
Alan Cheng <aaaalancheng@gmail.com>
2023-12-14 00:35:50 -0700

    Menu intro

---
+++ Assets/Scripts/MainMenuBackgroundScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuBackgroundScript : MonoBehaviour
+{
+    [SerializeField] protected GameObject mainMenu;
+    [SerializeField] protected GameObject headphones;
+
+    public void makeHeadphonesActive() {
+        headphones.SetActive(true);
+    }
+
+    public void makeMainMenuActive() {
+        headphones.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+}
