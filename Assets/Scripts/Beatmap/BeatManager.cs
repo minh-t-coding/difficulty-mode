1caa7b87e0e06cc7a993aed6bbb697e9b9b4d57a
simeonroth <simeonroth@email.arizona.edu>
2023-12-13 17:20:04 -0700

    tape stop effect

--- Assets/Scripts/Beatmap/BeatManager.cs
+++ Assets/Scripts/Beatmap/BeatManager.cs
@@ -100,6 +100,23 @@ public class BeatManager : MonoBehaviour {
             yield return Timing.WaitForSeconds(0.1f);
         }
         audioSource.Stop();
+        audioSource.volume = initVol;
+    }
+
+    public IEnumerator<float> FailAnim() {
+
+        float initVol = audioSource.volume;
+        float initPitch = audioSource.pitch;
+        int numSteps = 10;
+        SoundManager.Instance.playSound("recordScratch");
+        for(int i=0;i<numSteps;i++) {
+            audioSource.volume -= initVol/numSteps;
+            audioSource.pitch -= initPitch/numSteps;
+            yield return Timing.WaitForSeconds(0.1f);
+        }
+        audioSource.Stop();
+        audioSource.volume = initVol;
+        audioSource.pitch = initPitch;
     }
 
     public void constructBeatmap(Beatmap map) {
@@ -156,6 +173,8 @@ public class BeatManager : MonoBehaviour {
     
     protected bool hasLoadedFirstState = false;
     int currState = 1;
+
+    protected bool hasFailed = false;
     private void Update() {
 
         //foreach(Intervals interval in intervals) {
@@ -163,11 +182,13 @@ public class BeatManager : MonoBehaviour {
         //    interval.CheckForNewInterval(sampledTime);
         //}
 
-        if (songStarted && audioSource != null && AudioSettings.dspTime > startTimeOfOutro && audioSource.isPlaying) {
+        if (songStarted && !hasFailed && audioSource != null && AudioSettings.dspTime > startTimeOfOutro && audioSource.isPlaying) {
             if (numMissedBeats >= numLives) {
                 PlayerInputManager.Instance.setAllowedActions(new List<KeyCode>(), false);
                 Debug.Log("you lose >:(, restart nerd");
-                audioSource.Stop();
+                //audioSource.Stop();
+                hasFailed = true;
+                Timing.RunCoroutine(FailAnim().CancelWith(gameObject), this.gameObject.GetInstanceID());
                 return;
             }
             if (!hasLoadedFirstState) {
@@ -303,6 +324,7 @@ public class BeatManager : MonoBehaviour {
 
     public void triggerBeatmap(Beatmap map, SongObj song, AudioSource source, double startTime) {
         bar.SetActive(true);
+        hasFailed = false;
         PlayerInputManager.Instance.setIsStickoMode(true);
         audioSource = source;
         startTimeOfOutro = startTime;
