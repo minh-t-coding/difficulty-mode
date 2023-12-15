2ba79e4fafacfedbdec3810badbfd10342091c0d
simeonroth <simeonroth@email.arizona.edu>
2023-12-14 22:48:33 -0700

    demo soun fx

--- Assets/Scripts/AudioMenu.cs
+++ Assets/Scripts/AudioMenu.cs
@@ -6,12 +6,31 @@ using UnityEngine.Audio;
 public class AudioMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+
+    protected float currSfxVol;
+
+    protected bool hasSfxVolChanged;
+    void Start() {
+        currSfxVol = 0f;
+    }
     public void SetMusicVolume (float volume) {
         audioMixer.SetFloat("MusicParam", volume);
     }
 
     public void SetSFXVolume(float volume) {
+        if (currSfxVol!=volume) {
+            hasSfxVolChanged = true;
+        }
+        currSfxVol = volume;
         audioMixer.SetFloat("SFXParam", volume);
+        
+    }
+
+    void Update() {
+        if (Input.GetKeyUp(KeyCode.Mouse0) && hasSfxVolChanged) {
+            hasSfxVolChanged = false;
+            SoundManager.Instance.playSound("laser");
+        }
     }
 
     public void SetMasterVolume(float volume) {
